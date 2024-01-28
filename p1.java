75f7f3e4caf82a14481ebb882ea1bd2d4fe41ff5
kunalPW <kumbharekunal1825@gmail.com>
2024-01-28 15:22:01 +0530

    Age is add

--- p1.java
+++ p1.java
@@ -3,5 +3,6 @@ class p1
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("my name is kunal");
+        System.out.println("my age is 21 years old");
     }
 }
